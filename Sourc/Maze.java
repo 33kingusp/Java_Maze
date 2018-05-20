9112e89bd3a3b2afe893eb8795bf09cd4995579a
DeskTop_33kingusp <33kingusp@gmail.com>
2018-05-20 17:53:07 +0900

    迷路を生成できるようになった 引数1で正方形の迷路、引数2で長方形の迷路を生成できる。

--- Sourc/Maze.java
+++ Sourc/Maze.java
@@ -38,11 +38,83 @@ public class Maze
 					startList.add(new Vector2Int(x, y));
 			}
 		}
+		
+		while (startList.size() > 0)
+		{
+			int n = rand.nextInt(startList.size());
+			Vector2Int cell = startList.get(n);
+			startList.remove(n);
+			if (maze[cell.x][cell.y] == 0)
+			{
+				currentWallList.clear();
+				CreateWall(rand, cell.x, cell.y);
+			}
+		}
 	}
 
-	private void CreateWall(int x, int y)
+	private void CreateWall(Random rand, int x, int y)
 	{
+		List<Integer> dir = new ArrayList<Integer>();
+		if (maze[x][y - 1] == 0 && !IsCurrentWall(x, y - 2))
+			dir.add(0);
+		if (maze[x + 1][y] == 0 && !IsCurrentWall(x + 2, y))
+			dir.add(1);
+		if (maze[x][y + 1] == 0 && !IsCurrentWall(x, y + 2))
+			dir.add(2);
+		if (maze[x - 1][y] == 0 && !IsCurrentWall(x - 2, y))
+			dir.add(3);
 		
+		if (dir.size() > 0)
+		{
+			SetWall(x, y);
+			
+			boolean isPath = false;
+			int n = rand.nextInt(dir.size());
+			switch(dir.get(n))
+			{
+				case 0:
+					isPath = (maze[x][y - 2] == 0);
+					SetWall(x, --y);
+					SetWall(x, --y);
+					break;
+				case 1:
+					isPath = (maze[x + 2][y] == 0);
+					SetWall(++x, y);
+					SetWall(++x, y);
+					break;
+				case 2:
+					isPath = (maze[x][y + 2] == 0);
+					SetWall(x, ++y);
+					SetWall(x, ++y);
+					break;
+				case 3:
+					isPath = (maze[x - 2][y] == 0);
+					SetWall(--x, y);
+					SetWall(--x, y);
+					break;
+			}
+			if (isPath) CreateWall(rand, x, y);
+		}
+		else
+		{
+			Vector2Int before = PopCurrentWall();	
+			CreateWall(rand, before.x, before.y);
+		}	
+		
+	}
+
+	private void SetWall(int x, int y)
+	{
+		maze[x][y] = 1;
+		if (x % 2 ==0 && y % 2 == 0)
+			currentWallList.add(new Vector2Int(x, y));
+	}
+
+	private Vector2Int PopCurrentWall()
+	{
+		Vector2Int v = currentWallList.get(currentWallList.size() - 1);
+		currentWallList.remove(currentWallList.size() - 1);
+		return v;
 	}
 
 	private boolean IsCurrentWall(int x, int y)
@@ -61,8 +133,8 @@ public class Maze
 		for (int y = 0; y < size.y; y++)
 		{
 			for (int x = 0; x < size.x; x++)
-				if (maze[x][y] == 0) System.out.print(" ");
-				else System.out.print("*");
+				if (maze[x][y] == 0) System.out.print("  ");
+				else System.out.print("■");
 			System.out.println();
 		}
 	}
--- Sourc/MazeApp.java
+++ Sourc/MazeApp.java
@@ -2,7 +2,13 @@ class MazeApp
 {
 	public static void main(String args[])
 	{
-		Maze maze = new Maze(5, 5);
+		Vector2Int size = new Vector2Int(5, 5);
+		if (args.length >= 1)
+			size.x = size.y = Integer.parseInt(args[0]);
+		if(args.length >= 2)	
+			size.y = Integer.parseInt(args[1]);
+
+		Maze maze = new Maze(size.x, size.y);
 		maze.Print();
 
 		System.exit(0);
